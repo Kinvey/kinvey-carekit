014b77b102c1da79c2094006aa5b33f0d77d1a2c
Tejas <tejas@Tejass-MacBook-Pro.local>
2016-08-18 15:48:48 -0400

    changes for searching by activity and tests

---
+++ KinveyCareKit/PersOCKCarePlanActivity.swift
@@ -0,0 +1,9 @@
+//
+//  PersOCKCarePlanActivity.swift
+//  KinveyCareKit
+//
+//  Created by Tejas on 8/17/16.
+//  Copyright © 2016 Kinvey. All rights reserved.
+//
+
+import Foundation
---
+++ KinveyCareKit/UserInfoTransform.swift
@@ -0,0 +1,9 @@
+//
+//  UserInfoTransform.swift
+//  KinveyCareKit
+//
+//  Created by Tejas on 8/16/16.
+//  Copyright © 2016 Kinvey. All rights reserved.
+//
+
+import Foundation
